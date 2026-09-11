7416ba2d92101ac8d96f62f5b83f303556801ed8
Kyle Emmanuel <ke.domingz@gmail.com>
2026-09-11 08:51:59 +0800

    Product list: http client and ui improvements

--- app/src/main/java/com/example/yourcatalog/ui/screen/ProductListScreen.kt
+++ app/src/main/java/com/example/yourcatalog/ui/screen/ProductListScreen.kt
@@ -5,15 +5,20 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.input.clearText
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,6 +27,7 @@ import androidx.compose.runtime.ExperimentalComposeRuntimeApi
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.dp
@@ -29,11 +35,14 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
 import com.example.yourcatalog.data.model.Product
 import com.example.yourcatalog.ui.viewmodel.ProductsViewModel
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.debounce
 import org.koin.androidx.compose.koinViewModel
+import kotlin.time.Duration.Companion.milliseconds
 
-@OptIn(ExperimentalComposeRuntimeApi::class)
+@OptIn(ExperimentalComposeRuntimeApi::class, FlowPreview::class)
 @Composable
-fun ProductListScreen(modifier: Modifier = Modifier, viewModel: ProductsViewModel = koinViewModel()) {
+fun ProductListScreen(viewModel: ProductsViewModel = koinViewModel()) {
 
     val searchState = rememberTextFieldState()
     val state by viewModel.state.collectAsStateWithLifecycle()
@@ -41,6 +50,7 @@ fun ProductListScreen(modifier: Modifier = Modifier, viewModel: ProductsViewMode
     LaunchedEffect(searchState) {
 
         snapshotFlow { searchState.text }
+            .debounce(500.milliseconds)
             .collect { query ->
                 viewModel.search(query.toString())
             }
@@ -49,7 +59,17 @@ fun ProductListScreen(modifier: Modifier = Modifier, viewModel: ProductsViewMode
     Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
 
         Column(modifier = Modifier.padding(paddingValues)) {
-            OutlinedTextField(state = searchState)
+            OutlinedTextField(state = searchState,
+                modifier = Modifier.fillMaxWidth(),
+                trailingIcon = {
+                if (searchState.text.trim().isNotEmpty()) {
+                    IconButton(onClick = {
+                        searchState.clearText()
+                    }) {
+                        Icon(Icons.Filled.Clear, contentDescription = "clear search icon")
+                    }
+                }
+            })
             ProductList(products = state.products) { product ->
             }
         }
@@ -63,6 +83,7 @@ private fun ProductList(products: List<Product> = emptyList(), onProductTapped: 
             it.id
         }) { product ->
             ProductRow(product = product)
+            HorizontalDivider()
         }
     }
 }
@@ -73,7 +94,8 @@ private fun ProductRow(product: Product, onProductTapped: (Product) -> Unit = {}
     Row(
         modifier = Modifier.clickable {
             onProductTapped(product)
-        }
+        }.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         AsyncImage(
             model = product.thumbnail,
@@ -81,7 +103,7 @@ private fun ProductRow(product: Product, onProductTapped: (Product) -> Unit = {}
             modifier = Modifier.size(80.dp),
             contentDescription = "Product thumbnail"
         )
-        Column {
+        Column(verticalArrangement = Arrangement.Center) {
             Text(product.title)
             Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
                 Icon(Icons.Filled.Star, contentDescription = "Rating star")
